72e478290892c5eb79acc52c627d4e432eef5c92
helderseixas <helderseixas@gmail.com>
2020-02-13 09:42:39 -0300

    Cricacao da classe Livro

---
+++ IntroducaoOO/Livro.java
@@ -0,0 +1,11 @@
+class Livro{
+	String nome;
+	int anoPublicacao;
+	String autor;
+	String generos;
+	String editora;
+	String isbn;
+	float preco;
+	int unidadesAcervo;
+	int unidadesEmprestadas;
+}
