0d82a81b5c1b1852990bec9ae36d1086f9f4c325
MuriloNascimentoFerreira <murilobsi2018@gmail.com>
2020-02-14 10:10:51 -0300

    Implementado metodos

--- IntroducaoOO/Biblioteca.java
+++ IntroducaoOO/Biblioteca.java
@@ -10,16 +10,16 @@ class Biblioteca{
 		domCasmurro.isbn = "8572322647";
 		domCasmurro.preco = 26.10f;
 
-		Livro memoriaPostumas = new Livro();
-		memoriaPostumas.nome = "Memória Póstumas de Brás Cubas";
-		memoriaPostumas.anoPublicacao = 1881;
-		memoriaPostumas.autor = "Machado de Assis";
-		memoriaPostumas.generos = "Romance, Ficção";
-		memoriaPostumas.editora = "Oxford University Press";
-		memoriaPostumas.isbn = "9788496356979";
-		memoriaPostumas.preco = 27.92f;
+		Livro memoriasPostumas = new Livro();
+		memoriasPostumas.nome = "Memória Póstumas de Brás Cubas";
+		memoriasPostumas.anoPublicacao = 1881;
+		memoriasPostumas.autor = "Machado de Assis";
+		memoriasPostumas.generos = "Romance, Ficção";
+		memoriasPostumas.editora = "Oxford University Press";
+		memoriasPostumas.isbn = "9788496356979";
+		memoriasPostumas.preco = 27.92f;
 		
-		Livro cortico  = new Livro();
+		Livro cortico = new Livro();
 		cortico.nome = "O cortiço";
 		cortico.anoPublicacao = 1890;
 		cortico.autor = "Aluísio Azevedo";
@@ -27,5 +27,35 @@ class Biblioteca{
 		cortico.editora = "B. L. Garnier";
 		cortico.isbn = "9788888302423";
 		cortico.preco = 26.10f;
+				
+
+		System.out.println(domCasmurro.nome + " Acervo = " + domCasmurro.unidadesAcervo);
+		System.out.println(memoriasPostumas.nome + " Acervo = " + memoriasPostumas.unidadesAcervo);
+		System.out.println(cortico.nome + " Acervo = " + cortico.unidadesAcervo + "\n");
+		
+		//domCasmurro.adicionarNovasUnidades(10);
+		//cortico.adicionarNovasUnidades(4);
+		
+		//String dados = domCasmurro.recuperarDados();
+		//System.out.println(dados);
+		
+		boolean result = domCasmurro.emprestar();
+		if (result == true){	
+			System.out.println("Livro emprestado com sucesso!");
+		}else{
+			System.out.println("Impossível emprestar livro!");		
+		}
+
+
+
+
+
+
+
+
+
+
+
 	}	
+
 }
--- IntroducaoOO/Livro.java
+++ IntroducaoOO/Livro.java
@@ -6,6 +6,36 @@ class Livro{
 	String editora;
 	String isbn;
 	float preco;
-	int unidadesAcervo;
+	int unidadesAcervo = 1;
 	int unidadesEmprestadas;
+	
+	void adicionarNovasUnidades(int quantidade){
+		unidadesAcervo += quantidade;	
+	}
+	
+	String recuperarDados(){
+	String dados = "";
+	dados+= "Nome:" + nome + "\n" +
+	"Ano de publicação:" + anoPublicacao + "\n" + 
+	"Autor:" + autor + "\n" + 
+	"Generos:" + generos + "\n" +
+	"Editora:" + editora + "\n" +
+	"ISBN:" + isbn + "\n" +
+	"Preço:" + preco + "\n" +
+	"Unidades no acervo:" + unidadesAcervo + "\n"+
+	"Unidade emprestadas:" + unidadesEmprestadas;
+	
+	return dados;
+	}
+
+	boolean emprestar(){
+		if (unidadesAcervo > unidadesEmprestadas){
+			unidadesEmprestadas++;
+			return true;		
+		}
+		else{
+			return false;		
+		}
+	}
+
 }
